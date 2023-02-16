108f8964a632fbf4327cfacc3ee372dc19af9d4f
Usuario <Usuario@CPD03-M06>
2023-02-16 15:41:28 -0300

    Adicionar arquivos de projeto.

---
+++ AppTesteCep/AppTesteCep/Model/Bairro.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTesteCep.Model
+{
+    public class Bairro
+    {
+        public string Descricao_Bairro { get; set; }    
+    }
+}
