e03b0db4e75b2336d3f68f002f698c33cb0c9335
Usuario <Usuario@CPD02-M05>
2023-03-02 15:13:28 -0300

    atualizado

--- AppTesteCep/AppTesteCep/Model/Bairro.cs
+++ AppTesteCep/AppTesteCep/Model/Bairro.cs
@@ -9,3 +9,10 @@ namespace AppTesteCep.Model
         public string Descricao_Bairro { get; set; }    
     }
 }
+
+
+
+// https://cep.metodo
+
+// https://cep.metodo.com.br/logradouro/by-bairro?id_cidade=4874&bairro=jardim%20ameriaca
+// https://cep.metoda.com.br/cep/by-logradouro?logradouro=Rua
